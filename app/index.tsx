a64f24c10db083949e30a3c91c8cbf4f9edef82c
Jing Xuan Goh <gohjx8808@gmail.com>
2025-06-29 22:19:59 +0800

    added starter screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import typographyStyles from "@/assets/styles/typographyStyles";
+import { StyleSheet, View } from "react-native";
+import { IconButton, Text, useTheme } from "react-native-paper";
 
 export default function Index() {
+  const { colors } = useTheme();
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.rootContainer,
+        {
+          backgroundColor: colors.background,
+        },
+      ]}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text variant="displaySmall" style={typographyStyles.boldText}>
+        What&apos;s That?
+      </Text>
+      <Text variant="titleLarge" style={typographyStyles.centerText}>
+        Identify objects around you using your camera.
+      </Text>
+      <IconButton
+        icon="camera-outline"
+        size={100}
+        onPress={() => console.log("Pressed")}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 30,
+  },
+});
