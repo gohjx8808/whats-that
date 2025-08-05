34f7d4927f13320ba40a90665ec4de42928617af
Jing Xuan Goh <gohjx8808@gmail.com>
2025-08-05 20:18:46 +0800

    integrated bounding box

--- app/preview.tsx
+++ app/preview.tsx
@@ -1,24 +1,44 @@
 import { Image } from "expo-image";
 import { router, useLocalSearchParams } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Image as RNImage, ImageSize } from "react-native";
 import {
+  Detection,
   SSDLITE_320_MOBILENET_V3_LARGE,
   useObjectDetection,
 } from "react-native-executorch";
-import { Appbar, Button } from "react-native-paper";
+import { Appbar, Button, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Preview = () => {
   const params = useLocalSearchParams<{ imageUrl: string }>();
+  const test =
+    "https://i.guim.co.uk/img/media/327aa3f0c3b8e40ab03b4ae80319064e401c6fbc/377_133_3542_2834/master/3542.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=34d32522f47e4a67286f9894fc81c863";
   const ssdLite = useObjectDetection({
     modelSource: SSDLITE_320_MOBILENET_V3_LARGE,
   });
 
+  const [scale, setScale] = useState({ height: 0, width: 0 });
+  const [detectResult, setDetectResult] = useState<Detection[]>([]);
+  const [renderedTopPadding, setRenderedTopPadding] = useState(0);
+  const [actualImageDimension, setActualImageDimension] = useState<ImageSize>({
+    height: 0,
+    width: 0,
+  });
+
   const { bottom } = useSafeAreaInsets();
 
+  useEffect(() => {
+    const getImageDimension = async () => {
+      const dimension = await RNImage.getSize(test);
+      setActualImageDimension(dimension);
+    };
+    getImageDimension();
+  }, []);
+
   const detectObject = async () => {
-    const detection = await ssdLite.forward(params.imageUrl);
-    console.log(detection);
+    const detection = await ssdLite.forward(test);
+    setDetectResult(detection);
   };
 
   return (
@@ -28,11 +48,64 @@ const Preview = () => {
         <Appbar.Content title="Preview and Detection" />
       </Appbar.Header>
       <View style={[styles.contentContainer, { paddingBottom: bottom }]}>
-        <Image
-          source={params.imageUrl}
-          contentFit="contain"
-          style={styles.image}
-        />
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Image
+            source={test}
+            contentFit="contain"
+            style={[
+              styles.image,
+              {
+                aspectRatio:
+                  actualImageDimension.width / actualImageDimension.height,
+              },
+            ]}
+            onLayout={(event) => {
+              const { width, height, y } = event.nativeEvent.layout;
+              setRenderedTopPadding(y);
+              setScale({
+                width: width / actualImageDimension.width,
+                height: height / actualImageDimension.height,
+              });
+            }}
+          />
+        </View>
+        {detectResult.map((det, index) => {
+          const { x1, x2, y1, y2 } = det.bbox;
+          const box = {
+            x: x1 * scale.width,
+            y: y1 * scale.height,
+            width: (x2 - x1) * scale.width,
+            height: (y2 - y1) * scale.height,
+          };
+          return (
+            <View
+              key={index}
+              style={[
+                {
+                  position: "absolute",
+                  borderWidth: 2,
+                  borderColor: "red",
+                  left: box.x,
+                  top: box.y + renderedTopPadding,
+                  width: box.width,
+                  height: box.height,
+                },
+              ]}
+            >
+              <Text
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                  fontSize: 12,
+                  paddingHorizontal: 4,
+                  paddingVertical: 1,
+                }}
+              >
+                {`${det.label} (${(det.score * 100).toFixed(1)}%)`}
+              </Text>
+            </View>
+          );
+        })}
         <Button
           mode="contained"
           style={styles.detectButton}
@@ -55,7 +128,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   image: {
-    flex: 1,
     width: "100%",
   },
   detectButton: {
